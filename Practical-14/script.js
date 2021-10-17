0303d70dee139ac60c8c78ec2ccae0fdfed59ad5
aryanjais <aryan.jaiswal_cs19@gla.ac.in>
2021-10-17 13:02:55 +0530

    COMMIT

--- Practical-14/script.js
+++ Practical-14/script.js
@@ -1,12 +1,13 @@
 function add(){
     var value=document.getElementById('count');
     value.innerHTML=value.innerHTML*1+1;
+    document.getElementById('subtract').disabled = false;
 }
 function subtract(){
     var value=document.getElementById('count');
     if(value.innerHTML<=0){
-    value.innerHTML=0;
-    return;
+        document.getElementById('subtract').disabled = true;
+        return;
     }
     value.innerHTML=value.innerHTML-1;
 }
